e3c9e45024379bbc2f20e29c941a1a4006d776f3
Evgeniya Bayda <baje75128@gmail.com>
2021-07-31 21:30:41 +0300

    add

---
+++ index.js
@@ -0,0 +1,8 @@
+import { colors } from './js/colors';
+
+const refs = {
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]'),
+};
+console.log(refs.btnStart);
+console.log(refs.btnStop);
